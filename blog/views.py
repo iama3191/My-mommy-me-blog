477369b56dcfaa3c0196a56af180e9b0d5d409e9
iama3191 <ire.andreina.z@gmail.com>
2022-09-27 08:24:18 +0000

    Modify my method on my forms from recipe to post. Error from the last replace

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
+"""
+Views
+"""
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
 from django.http import HttpResponseRedirect
@@ -5,7 +8,39 @@ from .models import Recipe
 from .forms import CommentForm, RecipeForm
 
 
+def about(request):
+    """
+    Renders the about page
+    """
+    return render(request, "about.html")
+
+
+def add_recipe(request):
+    """
+    Renders add a recipe page 
+    """
+    recipe_form = RecipeForm(request.POST or None, request.FILES or None)
+    context = {
+        'recipe_form': recipe_form,
+    }
+
+    if request.method == 'POST':
+        recipe_form = RecipeForm(request.POST, request.FILES)
+        if recipe_form.is_valid():
+            recipe_form = recipe_form.save(commit=False)
+            recipe_form.author = request.user
+            recipe_form.status = 1
+            recipe_form.save()
+            return redirect('home')
+    else:
+        recipe_form = RecipeForm()
+    return render(request,'add_recipe.html', context)
+
+
 class RecipeList(generic.ListView):
+    """
+    This class creates the recipe list
+    """
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
@@ -13,7 +48,13 @@ class RecipeList(generic.ListView):
 
 
 class RecipeDetail(View):
+    """
+    This class creates the recipe view
+    """
     def get(self, request, slug, *args, **kwargs):
+        """
+        This class creates the recipe list
+        """
         queryset = Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
         comments = recipe.comments.order_by('created_on')
@@ -29,11 +70,15 @@ class RecipeDetail(View):
                 "comments": comments,
                 "commented": False,
                 "liked": liked,
-                "comment_form": CommentForm()
+                "comment_form": CommentForm(),
+                "recipe_form": RecipeForm(),
             },
         )
 
     def recipe(self, request, slug, *args, **kwargs):
+        """
+        Submits form content to the database
+        """
         queryset = Recipe.objects.filter(status=1)
         recipe = get_object_or_404(queryset, slug=slug)
         comments = recipe.comments.order_by('created_on')
@@ -41,7 +86,7 @@ class RecipeDetail(View):
         if recipe.likes.filter(id=self.request.user.id).exists():
             liked = True
 
-        comment_form = CommentForm(data=request.recipe)
+        comment_form = CommentForm(data=request.POST)
 
         if comment_form.is_valid():
             comment_form.instance.email = request.user.email
@@ -65,46 +110,18 @@ class RecipeDetail(View):
         )
 
 
-class RecipeLike(View):
-
-    def recipe(self, request, slug):
-        recipe = get_object_or_404(Recipe, slug=slug)
-        if recipe.likes.filter(id=request.user.id).exists():
-            recipe.likes.remove(request.user)
-        else:
-            recipe.likes.add(request.user)
-
-        return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
-
-
-def add_recipe(request):
-    recipe_form = RecipeForm(request.recipe or None, request.FILES or None)
-    context = {
-        'recipe_form': recipe_form,
-    }
-
-    if request.method == 'recipe':
-        recipe_form = RecipeForm(request.recipe, request.FILES)
-        if recipe_form.is_valid():
-            recipe_form = recipe_form.save(commit=False)
-            recipe_form.author = request.user
-            recipe_form.status = 1
-            recipe_form.save()
-            return redirect('home')
-    else:
-        recipe_form = RecipeForm()
-    return render(request,'add_recipe.html', context)
-
-
 def update_recipe(request, slug):
+    """
+    Recipe update view
+    """
     recipe = get_object_or_404(Recipe, slug=slug)
-    recipe_form = RecipeForm(request.recipe or None, instance=recipe)
+    recipe_form = RecipeForm(request.POST or None, instance=recipe)
     context = {
         'recipe_form': recipe_form,
         'recipe': recipe,
     }
-    if request.method == "recipe":
-        recipe_form = RecipeForm(request.recipe, request.FILES, instance=recipe)
+    if request.method == "POST":
+        recipe_form = RecipeForm(request.POST, request.FILES, instance=recipe)
         if recipe_form.is_valid():
             recipe = recipe_form.save(commit=False)
             recipe.author = request.user
@@ -116,10 +133,27 @@ def update_recipe(request, slug):
 
 
 def delete_recipe(request, slug):
+    """
+    Recipe delete view
+    """
     recipe = get_object_or_404(Recipe, slug=slug)
     recipe.delete()
     return redirect('home')
 
 
-def about(request):
-    return render(request, "about.html")
+class RecipeLike(View):
+    """
+    Likes on a recipe
+    """
+    def recipe(self, request, slug):
+        """
+        Submits to view
+        """
+        recipe = get_object_or_404(Recipe, slug=slug)
+        if recipe.likes.filter(id=request.user.id).exists():
+            recipe.likes.remove(request.user)
+        else:
+            recipe.likes.add(request.user)
+
+        return HttpResponseRedirect(reverse('recipe_detail', args=[slug]))
+
