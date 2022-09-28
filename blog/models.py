f76558cf141d6f41d0aa57b3f177050d45eabf95
iama3191 <ire.andreina.z@gmail.com>
2022-09-28 11:45:19 +0000

    Add tests to the test_model file: title is str, comment is str

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,14 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 
 class Recipe(models.Model):
+    """Data model for recipe"""
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_recipes")
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="blog_recipes"
+        )
     updated_on = models.DateTimeField(auto_now=True)
     ingredients = models.TextField()
     instructions = models.TextField(default="")
@@ -19,12 +24,11 @@ class Recipe(models.Model):
     likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
 
     class Meta:
+        """Sets the order of comments by date ascending"""
         ordering = ['-created_on']
 
     def __str__(self):
-        """
-        Return the title of the recipe
-        """
+        """Return the title of the recipe"""
         return self.title
 
     def number_of_likes(self):
@@ -32,7 +36,12 @@ class Recipe(models.Model):
 
 
 class Comment(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='comments')
+    """Data model for comments"""
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='comments'
+        )
     name = models.CharField(max_length=50)
     email = models.EmailField()
     body = models.TextField()
@@ -40,7 +49,9 @@ class Comment(models.Model):
     approved = models.BooleanField(default=True)
 
     class Meta:
+        """Sets the order of comments by date ascending"""
         ordering = ['created_on']
 
     def __str__(self):
+        """Returns comment with body and name"""
         return f"Comment {self.body} by {self.name}"
--- blog/test_models.py
+++ blog/test_models.py
@@ -3,11 +3,17 @@ from django.contrib.auth.models import User
 from .models import Recipe, Comment
 
 
-class TestModels(TestCase):
-    @classmethod
-    def setUpTestData(self):
+class TestAppModels(TestCase):
+    """
+    Test app models
+    """
+
+    def setUp(self):
+        """
+        Create data for testing
+        """
         self.user = User.objects.create(username='testname')
-        self.user.set_password('1234')
+        self.user.set_password('secret_password')
         self.user.save()
 
         self.recipe = Recipe.objects.create(
@@ -18,6 +24,21 @@ class TestModels(TestCase):
 
         self.comment = Comment.objects.create(
             recipe=self.recipe,
-            author=self.user,
-            body='test comment'
-        ) 
+            name=self.user,
+            body='test comment',
+        )
+
+    def test_recipe_model_str(self):
+        """
+        Test the __str__ method for recipe
+        """
+        self.assertEqual(self.recipe.__str__(), self.recipe.title)
+
+    def test_comment_model_str(self):
+        """
+        Test the __str__ method for comment
+        """
+        self.assertEqual(
+            self.comment.__str__(),
+            f"Comment {self.comment.body} by {self.comment.name}"
+            )
